4f5da0db8b4b1f693fd29d7ed618798307362364
beliemun <beliemun@paran.com>
2021-01-27 12:07:50 +0900

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,49 +20,36 @@ const routes = {
   },
   img: {
     src: "src/img/*",
-    dest: "build/img"
+    dest: "build/img",
   },
   scss: {
     watch: "src/scss/**/*.scss",
     src: "src/scss/styles.scss",
-    dest: "build/css"
+    dest: "build/css",
   },
   js: {
     watch: "src/js/**/*.js",
     src: "src/js/main.js",
-    dest: "build/js"
-  }
+    dest: "build/js",
+  },
 };
-
-export const pug = () =>
-  gulp
-    .src(routes.pug.src)
-    .pipe(gulpPug())
-    .pipe(gulp.dest(routes.pug.dest))
-    .pipe(connect.reload());
 
 // Task란 pug파일들은 HTML파일로 변환하거나, SCSS파일들을 CSS파일로 변환하거나,
 // 이미지를 최적화하고 JS를 압축하는 등의 말하고 각 Task를 나누거나 묶어서 만들 수 있다.
-
-export const clean = () => del(["build", ".publish"]);
-
-const webserver = () => {
-  gulp.src("build");
-  connect.server({
-    root: "build",
-    livereload: true,
-    port: 8000
-  });
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
-}
+const clean = () => del(["build", ".publish"]);
 
 const img = () =>
   gulp
     .src(routes.img.src)
     .pipe(gulpImage())
     .pipe(gulp.dest(routes.img.dest))
+    .pipe(connect.reload());
+
+const pug = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gulpPug())
+    .pipe(gulp.dest(routes.pug.dest))
     .pipe(connect.reload());
 
 const styles = () =>
@@ -80,29 +67,41 @@ const js = () =>
     .pipe(
       gulpBro({
         transform: [
-          babelify.configure({ presets: ['@babel/preset-env'] }),
-          ['uglifyify', { global: true }]
-        ]
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
       })
     )
     .pipe(gulp.dest(routes.js.dest))
     .pipe(connect.reload());
 
-const gh = () => gulp.src("build/**/*").pipe(phPages());
+const webserver = () => {
+  gulp.src("build");
+  connect.server({
+    root: "build",
+    livereload: true,
+    port: 8000,
+  });
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+};
 
-const detectChaged = () => {
-  gulp.watch(routes.pug.watch, pug);
+const detectChanged = () => {
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.scss.watch, styles);
   gulp.watch(routes.js.watch, js);
   return new Promise((resolve, reject) => {
     resolve();
-  })
-}
+  });
+};
 
-const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug, styles, js]);
-const live = gulp.series([webserver, detectChaged]);
+const gh = () => gulp.src("build/**/*").pipe(phPages());
+
+const prepare = gulp.series([clean]);
+const assets = gulp.series([img, pug, styles, js]);
+const live = gulp.series([webserver, detectChanged]);
 
 export const build = gulp.series([prepare, assets]);
 export const dev = gulp.series([build, live]);
